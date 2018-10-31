c3a5e77b6a8c0a381f4f866aae02729e65c1bd5c
Xioann02 <xioann02@cs.ucy.ac.cy>
2018-10-31 19:02:29 +0200

    Add files via upload
    Home Page Version 2:
    - Changes to banner ( mostly position -changes )
    -  Change vision board
    - Added Clients comments

---
+++ Assignment 2/js.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  $(".reviews").each(function(){
+    var This = $(this) ;
+    var Nums = This.find(".box").size() ;
+    This.find(".box:first").addClass("boxAct");
+    This.append("<div class='cnt'></div>") ;
+    This.find(".box").not(".boxAct")
+      .css("left","100%")
+    for ( i=0 ; i<Nums ; i++) {
+      This.find(".cnt").append("<span></span>") ;
+    }
+    This.find(".cnt span:eq(0)").addClass("ContActive");
+
+    This.find(".cnt span").click(Reviews);
+
+    function Reviews(){
+      var loc = $(this).index();
+      var ActivLoc = This.find(".ContActive").index();
+
+      $(this).addClass("ContActive")
+        .siblings().removeAttr("class");
+
+      if ( loc > ActivLoc ) {
+        var Dire = '100%'
+        var IDire = '-100%'
+      }
+      if ( loc < ActivLoc ) {
+        var Dire = '-100%'
+        var IDire = '100%'
+      }
+
+      This.find(".box").not(".boxAct")
+      .css("left",Dire) ;
+      This.find(".box:eq("+loc+")")
+      .animate({'left':'0'},speed)
+      .addClass("boxAct")
+      .siblings(".boxAct")
+      .removeClass("boxAct")
+      .animate({'left':IDire},speed);
+    }
+  });
+});
